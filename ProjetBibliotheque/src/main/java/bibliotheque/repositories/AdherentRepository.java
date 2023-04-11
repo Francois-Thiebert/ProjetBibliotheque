0ffa7147dff771eb49a96b4745730044bc0390dd
Alix Herbelot <alix.herbelot@gmail.com>
2023-04-11 14:49:33 +0200

    Ajout de repositories

--- ProjetBibliotheque/src/main/java/bibliotheque/repositories/AdherentRepository.java
+++ ProjetBibliotheque/src/main/java/bibliotheque/repositories/AdherentRepository.java
@@ -1,10 +1,19 @@
 package bibliotheque.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import bibliotheque.entities.Adherent;
 
 
 public interface AdherentRepository extends JpaRepository<Adherent, Long>{
-
+	@Query("select a from Adherent a.id=:id")
+	Optional<Adherent> findById(@Param("id") Long id);
+	
+	@Query("select a from Adherent a.nomUtilisateur=:nomUtilisateur")
+	Optional<Adherent> findByNomUtilisateur(@Param("nomUtilisateur") String nomUtilisateur);
 }
+
--- ProjetBibliotheque/src/main/java/bibliotheque/repositories/AdministrateurRepository.java
+++ ProjetBibliotheque/src/main/java/bibliotheque/repositories/AdministrateurRepository.java
@@ -1,10 +1,18 @@
 package bibliotheque.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import bibliotheque.entities.Administrateur;
 
 
 public interface AdministrateurRepository extends JpaRepository<Administrateur, Long>{
-
+	@Query("select a from Administrateur a.id=:id")
+	Optional<Administrateur> findById(@Param("id") Long id);
+	
+	@Query("select a from Administrateur a.nomUtilisateur=:nomUtilisateur")
+	Optional<Administrateur> findByNomUtilisateur(@Param("nomUtilisateur") String nomUtilisateur);
 }
