fce990c8152d647634e31bd8bccdc1092ad9ee51
Thiebert françois <fthiebert.poubelle@gmail.com>
2023-04-21 14:43:42 +0100

    test emprunt

--- ProjetBibliotheque/src/test/java/bibliotheque/EmpruntServiceTest.java
+++ ProjetBibliotheque/src/test/java/bibliotheque/EmpruntServiceTest.java
@@ -34,16 +34,23 @@ class EmpruntServiceTest {
 		empruntService.createOrUpdate(new Emprunt());
 	}
 	
+	@Disabled
+	@Test
+	@Commit
+	void supEmprunt(Emprunt emprunt) {
+		empruntService.delete(emprunt);
+	}
+	
 //	@Test
 //	void injectionEmpruntServiceTest() {
 //		assertNotNull(empruntService);
 //	}
 
-	@Test
-	void insertEmpruntTest() {
-		Emprunt e = new Emprunt(LocalDate.of(2023, 05, 13),LocalDate.now(),new Adherent(),new Livre());
-		empruntService.createOrUpdate(e);
-		assertNotNull(empruntService.getById(e.getId()));
-	}
+//	@Test
+//	void insertEmpruntTest() {
+//		Emprunt e = new Emprunt(LocalDate.of(2023, 05, 13),LocalDate.now(),new Adherent(),new Livre());
+//		empruntService.createOrUpdate(e);
+//		assertNotNull(empruntService.getById(e.getId()));
+//	}
 
 }
